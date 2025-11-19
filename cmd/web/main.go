849620b1f072287ea1826ca9534856e6fe78b3f2
prast13 <dwiprst13@gmail.com>
2025-11-19 23:07:12 +0700

    refactor: improve main.go structure

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,27 +2,19 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"kerjakuy/internal/router"
+	"kerjakuy/pkg/config"
 	"kerjakuy/pkg/database"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found, using system environment")
-	}
-
-	db := database.InitPostgresDB()
+	cfg := config.LoadConfig()
+	db := database.InitPostgresDB(cfg)
 
 	r := router.SetupRouter(db)
 
-	port := os.Getenv("APP_PORT")
-	if port == "" {
-		port = "8080"
-	}
+	port := cfg.AppPort
 
 	log.Printf("🚀 Starting server on :%s\n", port)
 
